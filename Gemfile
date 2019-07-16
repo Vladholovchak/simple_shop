cd9e3668b47866a7a9512f9a395fdb07dcd4f7fc
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-07-16 22:22:06 +0300

    add Versioning, install Device and DoorKeeper

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ group :test do
   gem 'faker'
   gem 'database_cleaner'
 end
-
+gem 'doorkeeper'
+gem 'devise'
 gem 'rubocop'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  use_doorkeeper
+  devise_for :users
+  scope module: :api, defaults: { format: :json }, path: 'api'  do
+    scope module: :v1, constraints: ApiVersion.new('v1', true) do
+      devise_for :users, controllers: {
+          registrations: 'api/v1/users/registrations',
+      }, skip: [:sessions, :password]
+    end
+  end
 end
